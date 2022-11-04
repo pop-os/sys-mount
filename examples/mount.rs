51c7d58195d95a606321dfd58a9b4f64b946aacc
Michael Aaron Murphy <mmstick@pm.me>
2022-11-04 15:58:54 +0100

    chore: Update examples

--- examples/mount.rs
+++ examples/mount.rs
@@ -1,21 +1,18 @@
 // Copyright 2018-2022 System76 <info@system76.com>
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT OR Apache-2.0
 
-extern crate clap;
-extern crate sys_mount;
+use clap::{Arg, Command};
+use std::process::ExitCode;
+use sys_mount::{Mount, SupportedFilesystems};
 
-use clap::{App, Arg};
-use std::process::exit;
-use sys_mount::{Mount, MountFlags, SupportedFilesystems};
-
-fn main() {
-    let matches = App::new("mount")
-        .arg(Arg::with_name("source").required(true))
-        .arg(Arg::with_name("directory").required(true))
+fn main() -> ExitCode {
+    let matches = Command::new("mount")
+        .arg(Arg::new("source").required(true))
+        .arg(Arg::new("directory").required(true))
         .get_matches();
 
-    let src = matches.value_of("source").unwrap();
-    let dir = matches.value_of("directory").unwrap();
+    let src = matches.get_one::<String>("source").unwrap();
+    let dir = matches.get_one::<String>("directory").unwrap();
 
     // Fetch a listed of supported file systems on this system. This will be used
     // as the fstype to `Mount::new`, as the `Auto` mount parameter.
@@ -23,19 +20,20 @@ fn main() {
         Ok(supported) => supported,
         Err(why) => {
             eprintln!("failed to get supported file systems: {}", why);
-            exit(1);
+            return ExitCode::FAILURE;
         }
     };
 
     // The source block will be mounted to the target directory, and the fstype is likely
     // one of the supported file systems.
-    match Mount::new(src, dir, &supported, MountFlags::empty(), None) {
+    match Mount::builder().fstype(&supported).mount(src, dir) {
         Ok(mount) => {
             println!("mounted {} ({}) to {}", src, mount.get_fstype(), dir);
+            ExitCode::SUCCESS
         }
         Err(why) => {
             eprintln!("failed to mount {} to {}: {}", src, dir, why);
-            exit(1);
+            ExitCode::FAILURE
         }
     }
 }
--- examples/umount.rs
+++ examples/umount.rs
@@ -1,32 +1,31 @@
 // Copyright 2018-2022 System76 <info@system76.com>
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: MIT OR Apache-2.0
 
 extern crate clap;
 extern crate sys_mount;
 
-use clap::{App, Arg};
-use std::process::exit;
+use clap::{Arg, Command};
+use std::process::ExitCode;
 use sys_mount::{unmount, UnmountFlags};
 
-fn main() {
-    let matches = App::new("umount")
-        .arg(Arg::with_name("lazy").short("l").long("lazy"))
-        .arg(Arg::with_name("source").required(true))
+fn main() -> ExitCode {
+    let matches = Command::new("umount")
+        .arg(Arg::new("lazy").short('l').long("lazy"))
+        .arg(Arg::new("source").required(true))
         .get_matches();
 
-    let src = matches.value_of("source").unwrap();
+    let src = matches.get_one::<String>("source").unwrap();
 
-    let flags = if matches.is_present("lazy") {
+    let flags = if matches.get_flag("lazy") {
         UnmountFlags::DETACH
     } else {
         UnmountFlags::empty()
     };
 
-    match unmount(src, flags) {
-        Ok(()) => (),
-        Err(why) => {
-            eprintln!("failed to unmount {}: {}", src, why);
-            exit(1);
-        }
-    }
+    let Err(why) = unmount(src, flags) else {
+        return ExitCode::SUCCESS;
+    };
+
+    eprintln!("failed to unmount {}: {}", src, why);
+    ExitCode::FAILURE
 }
